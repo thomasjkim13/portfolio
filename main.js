5ae46b0f964c10f3733b9a7ee71d8aca76e72c42
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-08-17 07:19:15 -0400

    JS navbar

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 'use strict';
 
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(window.navbarHeight);
   if(window.scrollY > navbarHeight) {
       navbar.classList.add('navbar-dark');
   } else {
