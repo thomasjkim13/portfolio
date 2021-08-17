bcd1f14c57f18da701ec05f90a1037a73dbb5e9b
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-08-17 07:54:46 -0400

    JS menu btn

--- main.js
+++ main.js
@@ -10,3 +10,25 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar-dark');
   }
 })
+
+// Handle scrolling when tappingo the navbar menu
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  // console.log(e.target.dataset.link);
+  // Scroll the element
+  const scrollTo = document.querySelector(link);
+  const top = scrollTo.offsetTop - navbarHeight < 0 ? 0 : scrollTo.offsetTop - navbarHeight + 16;  
+  const left = scrollTo.offsetLeft;
+  window.scrollTo({
+    top:top,
+    left:0,
+    behavior: 'smooth'
+  });
+  // scrollTo.scrollIntoView({ behavior: 'smooth' });
+})
